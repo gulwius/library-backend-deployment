f97a8a89eca99c0dc3b81eeedc0fead7b9e19b2a
Julius Palaganas <juliuspalaganass@gmail.com>
2025-12-17 16:17:09 +0800

    6th version

--- library/settings.py
+++ library/settings.py
@@ -260,8 +260,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # --- EMAIL SETTINGS (Keep your existing config) ---
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'adamconcepcion25@gmail.com'
-EMAIL_HOST_PASSWORD = 'iihc txjh wxhk dvqq'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
