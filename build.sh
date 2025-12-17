0188f3731f87a13c6fb4dc666791f61b6eb60f0f
Julius Palaganas <juliuspalaganass@gmail.com>
2025-12-17 15:42:47 +0800

    3rd version

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# --- ADD THIS LINE TO CREATE SUPERUSER AUTOMATICALLY ---
+# The "|| true" part prevents the deploy from failing if the user already exists
+python manage.py createsuperuser --noinput || true
